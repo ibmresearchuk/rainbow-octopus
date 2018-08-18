e5e9e5aa35d28702addc96a15ceb0f73e136f431
Kris Schultz <krisschultz@us.ibm.com>
2018-08-18 10:52:58 -0400

    Octopus now faces camera when placed or moved.

---
+++ RainbowOctopus/Assets/RainbowOctopus/Scripts/ARTapToPlace.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ARTapToPlace : MonoBehaviour
+{
+	public ARSessionOrigin sessionOrigin;
+
+	/// <summary>
+	/// The GameObject instance that should be moved into position when an
+	/// AR plane is tapped.
+	/// </summary>
+	public GameObject targetObject;
+
+	/// <summary>
+	/// Whether the object should be rotated to face the camera when it is placed.
+	/// </summary>
+	public bool turnObjectTowardsCamera = true;
+
+	static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+	private void Start()
+	{
+		targetObject.SetActive(false);
+	}
+
+
+	void Update()
+	{
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+         
+			if (sessionOrigin.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+			{
+            
+				Pose hitPose = hits[0].pose;
+
+				targetObject.SetActive(true);
+				targetObject.transform.position = hitPose.position;
+
+				Debug.Log("Tapped surface at: " + hitPose.position);
+
+				if (turnObjectTowardsCamera)
+				{
+					Vector3 facingDirection = (Camera.main.transform.position - targetObject.transform.position);
+					facingDirection.y = 0;
+					facingDirection.Normalize();
+					targetObject.transform.rotation = Quaternion.LookRotation(facingDirection);
+				}
+			}
+		}
+	}
+}
